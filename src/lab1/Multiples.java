916c6fe2576be889620956bc6d450cf096688b12
ShubhamMohta29 <shubham.mohta.2995@gmail.com>
2025-09-08 17:35:04 -0400

    Multiples code

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int num = 0;
+        for  (int i = 1; i <= 1000; i++) {if (i % 3 == 0 || i % 5 == 0){num++;}}
+        System.out.println(num);
+    }
+}
