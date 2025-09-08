e2e9e2e74533a8a6d3b0ebfca2cda7f36cf213d0
ShubhamMohta29 <shubham.mohta.2995@gmail.com>
2025-09-08 17:47:38 -0400

    extracted method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,8 +2,13 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        int num = multiples();
+        System.out.println(num);
+    }
+
+    private static int multiples() {
         int num = 0;
         for  (int i = 1; i <= 1000; i++) {if (i % 3 == 0 || i % 5 == 0){num++;}}
-        System.out.println(num);
+        return num;
     }
 }
