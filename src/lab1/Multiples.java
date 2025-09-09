e316d802c05cc80024112e0bc193d040073aeed5
ShubhamMohta29 <shubham.mohta.2995@gmail.com>
2025-09-08 21:59:28 -0400

    made arguments for function

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,13 +2,13 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int num = multiples();
+        int num = multiples(1000, 4, 4);
         System.out.println(num);
     }
 
-    private static int multiples() {
+    public static int multiples(int n, int a, int b) {
         int num = 0;
-        for  (int i = 1; i <= 1000; i++) {if (i % 3 == 0 || i % 5 == 0){num++;}}
+        for  (int i = 1; i < n; i++) {if (i % a == 0 || i % b == 0){num++;}}
         return num;
     }
 }
